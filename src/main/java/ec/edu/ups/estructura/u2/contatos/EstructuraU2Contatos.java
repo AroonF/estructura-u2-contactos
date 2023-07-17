9df928a49efdc0431447514f5c71885e5447a989
Zacky _47 <aroonfrnndz@gmaill.com>
2023-07-17 09:38:47 -0500

    first commit

---
+++ src/main/java/ec/edu/ups/estructura/u2/contatos/EstructuraU2Contatos.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ec.edu.ups.estructura.u2.contatos;
+
+/**
+ *
+ * @author aroon
+ */
+public class EstructuraU2Contatos {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
